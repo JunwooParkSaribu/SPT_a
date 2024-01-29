52eca3abedd52d10f4f4351ad359cb27987abbdb
JunwooParkSaribu <junwoo5071@gmail.com>
2024-01-29 15:11:39 +0100

    init

--- Bipartite_searching.py
+++ Bipartite_searching.py
@@ -1,8 +1,10 @@
 from scipy.optimize import linear_sum_assignment
 import numpy as np
 
+
 def hungarian_algo_max(cost):
     cost = -np.array(cost)
+    print(cost)
     row_ind, col_ind = linear_sum_assignment(cost)
     val = -cost[row_ind, col_ind].sum()
     return row_ind, col_ind, val

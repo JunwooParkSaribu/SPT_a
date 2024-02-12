01b200e72a229aff01b57af048cfce47f8cfcaa2
JunwooParkSaribu <junwoo5071@gmail.com>
2024-02-12 17:39:49 +0100

    init

--- main.py
+++ main.py
@@ -1000,7 +1000,7 @@ if __name__ == '__main__':
     var_parallel = True
     confidence = 0.99
     amp = 1.3
-    THRESHOLDS = None  # [8, 14.5]
+    THRESHOLDS = None  #[8, 14.5]
 
     snr = '7'
     density = 'low'
@@ -1033,7 +1033,7 @@ if __name__ == '__main__':
     print(f'Mean nb of molecules per frame: {mean_nb_per_time:.2f} molecules/frame')
 
     start_time = timer()
-    segment_distribution = distribution_segments(loc, time_steps=time_steps, lag=blink_lag,
+    segment_distribution = distribution_segments(read_localization(f'{WINDOWS_PATH}/my_test1/{scenario}_{snr}_low/localization.txt')[0], time_steps=time_steps, lag=blink_lag,
                                                  parallel=False)
     print(f'Segmentation duration: {timer() - start_time:.2f}s')
     bin_size = np.mean(xyz_max - xyz_min) / 5000.
